d6b3ccb990565f47d5b89a23ef53fdb3a0f08ae6
kuchu13 <105021033@live.asia.edu.tw>
2017-11-05 23:21:04 +0800

    0926hw

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,12 +6,17 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
-    private  Button btnAdd = new Button("left");
-    private  Button btnSub = new Button("right");
-    private  Button btnStop = new Button("Fire");
-    private  Button btnExit = new Button("Exit");
-    private Label lab = new Label("SUN");
-    private int n = 0,labX=185,labY=50;
+    private  Button btnLeft = new Button("left");
+    private  Button btnRight = new Button("right");
+    private  Button btnFire = new Button("Fire");
+    private  Button btnStop = new Button("Stop");
+    private  Button btnAuto = new Button("Automove");
+
+    private Label lab1 = new Label("SUN");
+    private Label lab2 = new Label("GUN");
+
+    private int n = 0,labX=185,labY=50,labXx=185,labYy=150;
+
     private Timer t1;
     private Timer t2;
 
@@ -21,63 +26,67 @@ public class MainFrame extends Frame {
     private void initComp(){
         this.setBounds(100,120,400,300);
         this.setLayout(null);
+        MainFrame.this.setTitle("Game");
 
 
-        btnAdd.setBounds(70,200,80,30);
-        this.add(btnAdd);
-        btnAdd.addActionListener(new ActionListener() {
+        btnLeft.setBounds(70,200,80,30);
+        this.add(btnLeft);
+        btnLeft.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MainFrame.this.setTitle(Integer.toString(++n));
-//                lab.setText(Integer.toString(n));
-                t1.start();
+                lab2.setLocation(labXx -=10,labYy);
+
             }
         });
 
-
-        btnSub.setBounds(250,200,80,30);
-        this.add(btnSub);
-        btnSub.addActionListener(new ActionListener() {
+        btnRight.setBounds(250,200,80,30);
+        this.add(btnRight);
+        btnRight.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MainFrame.this.setTitle(Integer.toString(--n));
-//                lab.setText(Integer.toString(n));
-                t2.start();
+                lab2.setLocation(labXx +=10,labYy);
             }
         });
 
+        btnFire.setBounds(160,200,80,30);
+        this.add(btnFire);
 
-        btnStop.setBounds(160,200,80,30);
+
+
+
+        btnStop.setBounds(310,260,80,30);
         this.add(btnStop);
         btnStop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 t1.stop();
                 t2.stop();
+
             }
         });
 
-
-        btnExit.setBounds(150,250,80,30);
-        this.add(btnExit);
-        btnExit.addActionListener(new ActionListener() {
+        btnAuto.setBounds(10,260,80,30);
+        this.add(btnAuto);
+        btnAuto.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                t2.start();
             }
         });
 
 
-        lab.setBounds(185,50,80,30);
-        this.add(lab);
+        lab1.setBounds(185,50,80,30);
+        this.add(lab1);
 //            lab.setText(Integer.toString(lab.getX())+","+Integer.toString(lab.getY()));
+        lab2.setBounds(185,150,80,30);
+        this.add(lab2);
 
 
         t1=new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 labX +=5;
-                lab.setLocation(labX,labY);
+                lab1.setLocation(labX,labY);
                 if(labX>=370){
                     t1.stop();
                     t2.start();
@@ -88,7 +97,7 @@ public class MainFrame extends Frame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 labX -=5;
-                lab.setLocation(labX,labY);
+                lab1.setLocation(labX,labY);
                 if(labX<=0) {
                     t2.stop();
                     t1.start();
